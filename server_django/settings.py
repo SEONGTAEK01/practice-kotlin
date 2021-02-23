bf7114df34fd23a5d2fce3f91354679aceb58602
seongtaek.hong <seongtaek.hong@looxidlabs.com>
2021-02-23 11:20:29 +0900

    Reconfigure setting files location

--- server_django/settings.py
+++ server_django/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'server_django.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'server_django.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
